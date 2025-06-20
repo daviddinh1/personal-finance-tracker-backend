1561693addd0e4798a0e7b08731c0a7e78b939b6
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2025-06-20 15:56:10 -0500

    feat(auth): added User JPA entity with email, passHash, and creation timestamp

---
+++ src/main/java/org/dtd6003/personalfinancetrackerbackend/auth/model/User.java
@@ -0,0 +1,61 @@
+package org.dtd6003.personalfinancetrackerbackend.auth.model;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name="pass_hash", nullable=false)
+    private String passHash;
+
+    @CreationTimestamp
+    @Column(name="created_at", nullable=false, updatable=false)
+    private LocalDateTime createdAt;
+
+
+    protected User(){};
+
+    //declare construtor for beans
+    public User(Long id, String email, String passHash){
+        this.id = id;
+        this.email = email;
+        this.passHash = passHash;
+    }
+
+    //set getters
+    public Long getId(){
+        return this.id;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public String getPassHash(){
+        return this.passHash;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return this. createdAt;
+    }
+
+    //set setters
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    //im assuming generate password hash function here?
+    public void setPassHash(String passHash){
+        this.passHash = passHash;
+    }
+}
