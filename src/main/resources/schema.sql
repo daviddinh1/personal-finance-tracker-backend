6208f1f346e243fb428cb8318e4a7481ccf2dc7c
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2025-06-24 16:45:01 -0500

    feat(transaction): updated schema to allow for transactions storage

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,14 @@ CREATE TABLE users(
     pass_hash VARCHAR(500) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW()
 );
+
+CREATE TYPE txn_type AS ENUM('INCOME','EXPENSE');
+
+CREATE TABLE transactions(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    type txn_type NOT NULL,
+    amount DECIMAL(12,2) NOT NULL,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT NOW(),
+);
