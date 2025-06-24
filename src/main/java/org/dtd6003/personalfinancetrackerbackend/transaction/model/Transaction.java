9a111da36568481e59e4717fe88aa328e2288a9b
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2025-06-24 17:08:03 -0500

    feat(transaction): created entity for JPA and ENUM type

---
+++ src/main/java/org/dtd6003/personalfinancetrackerbackend/transaction/model/Transaction.java
@@ -0,0 +1,86 @@
+package org.dtd6003.personalfinancetrackerbackend.transaction.model;
+
+import jakarta.persistence.*;
+import org.dtd6003.personalfinancetrackerbackend.auth.model.User;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // maps to user_id FK
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="type", nullable=false)
+    private TxnType type;
+
+    @Column(name="description")
+    private String description;
+
+    @Column(name="amount", precision=12, scale=2, nullable=false)
+    private BigDecimal amount;
+
+    @CreationTimestamp
+    @Column(name="created_at", nullable=false, updatable=false)
+    private LocalDateTime createdAt;
+
+    protected Transaction(){}
+
+    public Transaction(User user,TxnType type, String description, BigDecimal amount){
+        this.user = user;
+        this.type = type;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    //set getters and setters
+    public long getId(){
+        return this.id;
+    }
+
+    public User getUser(){
+        return this.user;
+    }
+
+    public TxnType getType(){
+        return this.type;
+    }
+
+    public String getDescription(){
+        return this.description;
+    }
+
+    public BigDecimal getAmount(){
+        return this.amount;
+    }
+
+    public LocalDateTime getCreatedAt(){
+        return this.createdAt;
+    }
+
+    public void setUser(User user){
+        this.user = user;
+    }
+
+    public void setType(TxnType type){
+        this.type = type;
+    }
+
+    public void setAmount(BigDecimal amount){
+        this.amount = amount;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+}
---
+++ src/main/java/org/dtd6003/personalfinancetrackerbackend/transaction/model/TxnType.java
@@ -0,0 +1,7 @@
+package org.dtd6003.personalfinancetrackerbackend.transaction.model;
+
+public enum TxnType {
+    INCOME,
+    EXPENSE
+    // add more types if needed
+}
