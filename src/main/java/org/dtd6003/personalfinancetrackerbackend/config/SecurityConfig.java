fe0f60151f8f95dc96057f727a704e757f8c9abd
daviddinh1 <91696365+daviddinh1@users.noreply.github.com>
2025-06-26 13:26:06 -0500

    feat(transaction): filter now works before any request

--- src/main/java/org/dtd6003/personalfinancetrackerbackend/config/SecurityConfig.java
+++ src/main/java/org/dtd6003/personalfinancetrackerbackend/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package org.dtd6003.personalfinancetrackerbackend.config;
 
+import org.dtd6003.personalfinancetrackerbackend.transaction.security.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -26,12 +31,13 @@ public class SecurityConfig {
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
 
+                .addFilterBefore(jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class)
+
                 // disable default form login & HTTP Basic
                 .httpBasic(httpBasic -> httpBasic.disable())
-                .formLogin(form -> form.disable())
+                .formLogin(form -> form.disable());
 
-        // NOTE: you'll need to add your JWT filter here if you want to protect other endpoints
-        ;
 
         return http.build();
     }
